315979464a5cc1e8ac1bb0d39af2f4a5db863eb9
Kobe Mertens <kobe.mertens@hotmail.com>
2024-11-22 13:42:51 +0100

    add build thing for ember concurrency

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    babel: {
+      plugins: [
+        // ... any other plugins
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+
+        // NOTE: put any code coverage plugins last, after the transform.
+      ],
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
