a131a21bd58f7428eab45f290ff39eff89eb88d2
Erika Pauwels <erika.pauwels@gmail.com>
2024-11-27 08:41:53 +0100

    Add Bootstrap icons using ember-svg-jar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    svgJar: {
+      sourceDirs: [
+        'node_modules/bootstrap-icons/icons',
+      ],
+    },
     babel: {
       plugins: [
         // ... any other plugins
