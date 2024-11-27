8161d13dd34d04e1305e32f84c443ec59a20c4d9
Erika Pauwels <erika.pauwels@gmail.com>
2024-11-27 12:13:11 +0100

    Workaround Virtuoso date filter issue
    It seems Virtuoso struggles with date filter combination <= and >= on
    the same property. Therefore the filter to get the relevant worklogs
    for a month is refactored to a filter combining < with >=

--- app/routes/year/month.js
+++ app/routes/year/month.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { startOfMonth, addMonths } from 'date-fns';
 import { formatDate } from 'frontend-timekeeper/utils/format-date';
 
 export default class YearMonthRoute extends Route {
@@ -17,13 +18,11 @@ export default class YearMonthRoute extends Route {
   async model(params) {
     const { year } = this.modelFor('year');
     const monthNumber = Number(params.month) - 1;
-    // TODO use date-fns lib to avoid edge cases / timezone issue
-    // E.g. https://date-fns.org/v4.1.0/docs/startOfMonth
-    const firstOfMonth = new Date(Date.UTC(year, monthNumber));
-    const lastOfMonth = new Date(Date.UTC(year, monthNumber + 1, 0));
+    const firstOfMonth = startOfMonth(Date.UTC(year, monthNumber));
+    const firstOfNextMonth = addMonths(firstOfMonth, 1);
     const workLogs = await this.store.queryAll('work-log', {
       'filter[:gte:date]': formatDate(firstOfMonth),
-      'filter[:lte:date]': formatDate(lastOfMonth),
+      'filter[:lt:date]': formatDate(firstOfNextMonth),
       include: 'task',
     });
 
