c3f1078c9c7bd6b1630856660fc74a4d173926b0
Kobe Mertens <kobe.mertens@hotmail.com>
2024-11-28 12:28:43 +0100

    add login page

---
+++ app/components/mu-login.js
@@ -0,0 +1,5 @@
+import BaseMuLoginComponent from 'ember-mu-login/components/mu-login';
+import { action } from '@ember/object';
+
+export default class MuLoginComponent extends BaseMuLoginComponent {
+}
