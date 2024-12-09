9a888149f40f3c4ecf34f80ebd60cf70ac88f5b6
Kobe Mertens <kobe.mertens@hotmail.com>
2024-12-09 15:23:54 +0100

    fetch timesheets in year route

--- app/routes/year.js
+++ app/routes/year.js
@@ -1,23 +1,35 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { startOfYear } from 'date-fns';
+import { formatDate } from 'frontend-timekeeper/utils/format-date';
 
 export default class YearRoute extends Route {
   @service router;
   @service session;
+  @service store;
 
   beforeModel(transition) {
-    this.session.requireAuthentication(transition, 'login');
-  }
+    let { year } = this.paramsFor(this.routeName);
+    year = Number(year);
 
-  model(params) {
-    return {
-      year: Number(params.year),
-    };
-  }
-
-  afterModel({ year }) {
     if (isNaN(year) || year < 0) {
       this.router.transitionTo('404');
     }
+
+    this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model(params) {
+    const year = Number(params.year);
+    const firstOfYear = startOfYear(new Date(year, 0));
+    const firstOfNextYear = startOfYear(new Date(year + 1, 0));
+    const timesheets = await this.store.query('timesheet', {
+      'filter[:gte:start]': formatDate(firstOfYear),
+      'filter[:lt:start]': formatDate(firstOfNextYear),
+    });
+    return {
+      year: Number(params.year),
+      timesheets,
+    };
   }
 }
