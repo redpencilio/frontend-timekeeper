9960888fe4bd856bbb08446675009f0b49bb7e06
Kobe Mertens <kobe.mertens@hotmail.com>
2024-12-09 13:07:33 +0100

    add timesheet model

---
+++ app/models/timesheet.js
@@ -0,0 +1,23 @@
+import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
+import { TIMESHEET_STATUSES } from '../constants';
+
+export default class TimesheetModel extends Model {
+  @attr status;
+  @attr('date') start;
+  @attr('date') end;
+
+  @hasMany('work-log', { async: true, inverse: 'timesheet' }) workLogs;
+
+  get isDraft() {
+    return this.status === TIMESHEET_STATUSES.DRAFT;
+  }
+  get isAbsenceSubmitted() {
+    return this.status === TIMESHEET_STATUSES.ABSENCE_SUBMITTED;
+  }
+  get isSubmitted() {
+    return this.status === TIMESHEET_STATUSES.SUBMITTED;
+  }
+  get isExported() {
+    return this.status === TIMESHEET_STATUSES.EXPORTED;
+  }
+}
