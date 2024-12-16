ce818164f2ea00f61dfbdbc9f11e89582ed5511a
Kobe Mertens <kobe.mertens@hotmail.com>
2024-12-16 13:21:33 +0100

    show the user's name

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
 import { monthsInYear } from 'date-fns/constants';
 import { format } from 'date-fns';
+import { service } from '@ember/service';
 
 export default class NavBarComponent extends Component {
+  @service userProfile; 
+
   get months() {
     const items = [];
     for (let i = 0; i < monthsInYear; i++) {
