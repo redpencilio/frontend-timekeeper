f9cc5807b48c48f7beac0c1d941acddad31b2bbe
Kobe Mertens <kobe.mertens@hotmail.com>
2024-12-30 09:41:20 +0100

    take current timesheet submission status into account

--- app/components/timesheet-actions.js
+++ app/components/timesheet-actions.js
@@ -1,32 +1,38 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import constants from 'frontend-timekeeper/constants';
 const { TIMESHEET_STATUSES } = constants;
 
 export default class TimesheetActionsComponent extends Component {
-  @tracked absenceComplete = false;
-  @tracked timesheetComplete = false;
+  get absenceComplete() {
+    return [
+      TIMESHEET_STATUSES.ABSENCE_SUBMITTED,
+      TIMESHEET_STATUSES.SUBMITTED,
+      TIMESHEET_STATUSES.EXPORTED,
+    ].includes(this.args.timesheet.status);
+  }
+
+  get timesheetComplete() {
+    return [TIMESHEET_STATUSES.SUBMITTED, TIMESHEET_STATUSES.EXPORTED].includes(
+      this.args.timesheet.status,
+    );
+  }
 
   @action
   onAbsenceClick(checked) {
     if (!checked) {
-      this.timesheetComplete = false;
       this.args.onStatusChanged(TIMESHEET_STATUSES.DRAFT);
     } else {
       this.args.onStatusChanged(TIMESHEET_STATUSES.ABSENCE_SUBMITTED);
     }
-    this.absenceComplete = checked;
   }
 
   @action
   onTimesheetClick(checked) {
     if (checked) {
-      this.absenceComplete = true;
       this.args.onStatusChanged(TIMESHEET_STATUSES.SUBMITTED);
     } else {
       this.args.onStatusChanged(TIMESHEET_STATUSES.ABSENCE_SUBMITTED);
     }
-    this.timesheetComplete = checked;
   }
 }
