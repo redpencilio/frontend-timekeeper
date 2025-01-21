aca7f855d7d3ded6f7122afebf698847b94cfbc9
Kobe Mertens <kobe.mertens@hotmail.com>
2025-01-21 16:05:54 +0100

    remove permission check

--- app/routes/admin/timesheets/year/month.js
+++ app/routes/admin/timesheets/year/month.js
@@ -1,16 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import CONSTANTS from 'frontend-timekeeper/constants';
 
 export default class AdminTimesheetsYearMonthRoute extends Route {
   @service store;
   @service router;
-  @service userProfile;
 
   beforeModel(transition) {
-    if (!this.userProfile.may('manage-users')) {
-      this.router.transitionTo('forbidden');
-    }
     // Check if month is a valid number
     this.humanMonth = Number(transition.to.params.month);
     if (isNaN(this.humanMonth) || this.humanMonth < 1 || this.humanMonth > 12) {
