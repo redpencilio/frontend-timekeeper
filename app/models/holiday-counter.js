219b85c40addbc303499aa5c5ddaf46b66e29210
Kobe Mertens <kobe.mertens@hotmail.com>
2025-02-13 15:15:41 +0100

    add holiday-counter model

---
+++ app/models/holiday-counter.js
@@ -0,0 +1,10 @@
+import Model, { attr, belongsTo } from '@ember-data/model';
+
+export default class HolidayCounterModel extends Model {
+  @attr('duration') value;
+  @attr('date') validFrom;
+  @attr('date') validTo;
+
+  @belongsTo('person', { async: true, inverse: 'holidayCounters' }) person;
+  @belongsTo('concept', { async: true }) holidayType;
+}
