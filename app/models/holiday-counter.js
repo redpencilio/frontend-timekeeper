d06a986a49df8b897fcd68b31532dabcdb8985d6
Kobe Mertens <kobe.mertens@hotmail.com>
2025-02-18 16:42:38 +0100

    fix misnamed holiday-counter attr

--- app/models/holiday-counter.js
+++ app/models/holiday-counter.js
@@ -3,7 +3,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class HolidayCounterModel extends Model {
   @attr('duration') value;
   @attr('date') validFrom;
-  @attr('date') validTo;
+  @attr('date') validTill;
 
   @belongsTo('person', { async: true, inverse: 'holidayCounters' }) person;
   @belongsTo('concept', { async: true }) holidayType;
