1ae38b46e6cf7b80c798335de2ac4e73bc34ea74
Kobe Mertens <kobe.mertens@hotmail.com>
2025-02-20 14:13:02 +0100

    add initializer for noteContent

--- app/components/work-log-notes-popover.js
+++ app/components/work-log-notes-popover.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import { localCopy } from 'tracked-toolbox';
 
 export default class WorkLogNotesPopoverComponent extends Component {
-  @localCopy('args.workLog.note') noteContent;
+  @localCopy('args.workLog.note', '') noteContent;
 
   @action
   saveNote(event) {
