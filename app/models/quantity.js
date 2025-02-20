3958bdd04b33ae88262d091073aeb719231d0126
Kobe Mertens <kobe.mertens@hotmail.com>
2025-02-20 10:27:45 +0100

    use quantity instead of holiday counter

--- app/models/quantity.js
+++ app/models/quantity.js
@@ -3,8 +3,8 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class HolidayCounterModel extends Model {
   @attr('duration') value;
   @attr('date') validFrom;
-  @attr('date') validTo;
+  @attr('date') validTill;
 
-  @belongsTo('person', { async: true, inverse: 'holidayCounters' }) person;
-  @belongsTo('concept', { async: true }) holidayType;
+  @belongsTo('person', { async: true, inverse: 'quantities' }) person;
+  @belongsTo('concept', { async: true }) quantityKind;
 }
