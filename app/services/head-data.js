67ea4d27e2410d434789aefa218984465d2b8099
Kobe Mertens <kobe.mertens@hotmail.com>
2025-02-20 14:26:49 +0100

    Revert "add separate favicon for development environment"
    This reverts commit 912ac950eeb702b6a7c6e1f56f95c37116189a10.

--- app/services/head-data.js
+++
@@ -1,9 +0,0 @@
-import Service from '@ember/service';
-import ENV from 'frontend-timekeeper/config/environment';
-
-export default class HeadDataService extends Service {
-  favicon =
-    ENV.environment === 'development'
-      ? '/assets/favicon-dev.ico'
-      : '/assets/favicon.ico';
-}
