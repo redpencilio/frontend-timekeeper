23654cb5f7103fc5cd1678e53499db97c6be72f4
Kobe Mertens <kobe.mertens@hotmail.com>
2025-03-10 13:30:28 +0100

    add hide all/show all button

--- app/components/admin/task-visibility.js
+++ app/components/admin/task-visibility.js
@@ -6,7 +6,7 @@ import { keepLatest } from 'reactiveweb/keep-latest';
 
 export default class AdminTaskVisibilityComponent extends Component {
   @service store;
-  @service tasks;
+  @service('tasks') tasksService;
 
   @use tasksForUserLatest = keepLatest({
     value: () => this.tasksForUser.value,
@@ -39,32 +39,63 @@ export default class AdminTaskVisibilityComponent extends Component {
   };
 
   hideCustomer = async (customerWithTasks) => {
-    await Promise.all(customerWithTasks.tasks.map(this.hideTask));
+    const subTasks = customerWithTasks.tasks
+      .map(({ subTasks }) => subTasks)
+      .flat();
+    await this.submitHideSubTasks(subTasks);
   };
 
   hideTask = async (taskWithSubTasks) => {
-    await Promise.all(taskWithSubTasks.subTasks.map(this.hideSubTask));
+    await this.submitHideSubTasks(taskWithSubTasks.subTasks);
   };
 
   hideSubTask = async (subTask) => {
-    const visibleTasks = await this.args.person.tasks;
-    visibleTasks.removeObject(subTask);
-    await Promise.all([subTask.save(), this.args.person.save()]);
+    await this.submitHideSubTasks([subTask]);
+  };
+
+  submitHideSubTasks = async (subTasks) => {
+    const visibleTasks = await this.store.queryAll('task', {
+      'filter[visible-to][:id:]': this.args.person.id,
+    });
+    this.args.person.tasks = visibleTasks
+      .slice()
+      .filter(
+        (visibleTask) => !subTasks.find((task) => task.id === visibleTask.id),
+      );
+    await this.args.person.save();
   };
 
   showCustomer = async (customerWithTasks) => {
-    await Promise.all(customerWithTasks.tasks.map(this.showTask));
+    const subTasks = customerWithTasks.tasks
+      .map(({ subTasks }) => subTasks)
+      .flat();
+    await this.submitShowSubTasks(subTasks);
   };
 
   showTask = async (taskWithSubTasks) => {
-    await Promise.all(taskWithSubTasks.subTasks.map(this.showSubTask));
+    await this.submitShowSubTasks(taskWithSubTasks.subTasks);
   };
 
   showSubTask = async (subTask) => {
-    const visibleTasks = await this.args.person.tasks;
-    visibleTasks.addObject(subTask);
-    await Promise.all([subTask.save(), this.args.person.save()]);
+    await this.submitShowSubTasks([subTask]);
   };
+
+  submitShowSubTasks = async (subTasks) => {
+    const visibleTasks = await this.store.queryAll('task', {
+      'filter[visible-to][:id:]': this.args.person.id,
+    });
+    this.args.person.tasks = visibleTasks.slice().concat(subTasks);
+
+    await this.args.person.save();
+  };
+
+  showAllTasks = async () => {
+    await this.submitShowSubTasks(this.tasksService.tasks.slice());
+  };
+
+  hideAllTasks = async () => {
+    await this.submitHideSubTasks(this.tasksService.tasks.slice());
+  }
 
   // TODO: Maybe we need to put this in the model file?
   isSingleGeneral = ({ subTasks }) =>
