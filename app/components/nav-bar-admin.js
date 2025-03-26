fa1b5e969a7633029a8ce1548bec00e1693a5777
Erika Pauwels <erika.pauwels@gmail.com>
2025-03-26 16:47:33 +0100

    Add button to leave admin area

--- app/components/nav-bar-admin.js
+++ app/components/nav-bar-admin.js
@@ -3,4 +3,12 @@ import { service } from '@ember/service';
 
 export default class NavBarAdminComponent extends Component {
   @service userProfile;
+
+  get currentYear() {
+    return new Date().getFullYear();
+  }
+
+  get currentMonth() {
+    return new Date().getMonth() + 1;
+  }
 }
