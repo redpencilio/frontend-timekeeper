c454fc2be613b1444e8aff004d11ba999b26468d
Erika Pauwels <erika.pauwels@gmail.com>
2025-03-26 10:34:16 +0100

    Prettier fixes

--- app/components/holidays-overview.js
+++ app/components/holidays-overview.js
@@ -37,7 +37,7 @@ export default class HolidaysOverviewComponent extends Component {
   @use holidaysUsedLatest = keepLatest({
     value: () => this.holidaysUsed.value,
     when: () => this.holidaysUsed.isLoading,
-  })
+  });
 
   get sortedCounters() {
     return (this.args.holidayCounters ?? [])
