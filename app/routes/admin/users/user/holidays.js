b3e987472daee1c07f2d8fd2527539dcf0a1c760
Erika Pauwels <erika.pauwels@gmail.com>
2025-03-26 15:53:11 +0100

    Remove unused intermediate route

--- app/routes/admin/users/user/holidays.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class AdminUsersUserHolidaysRoute extends Route {
-  model() {
-    return { user: this.modelFor('admin.users.user') };
-  }
-}
