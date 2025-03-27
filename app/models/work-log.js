f36094de1378158bd4e7314b533ac7484bf6d97c
Kobe Mertens <kobe.mertens@hotmail.com>
2025-03-27 14:53:33 +0100

    put cloning functionality closer to the model so we don't forget to update

--- app/models/work-log.js
+++ app/models/work-log.js
@@ -1,6 +1,7 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class TimeLogModel extends Model {
+  // !! Don't forget to update `clone` when adding new attributes or relations
   @attr('string') uri;
   @attr('duration') duration;
   @attr('date') date;
@@ -9,4 +10,22 @@ export default class TimeLogModel extends Model {
   @belongsTo('task', { async: true, inverse: 'workLogs' }) task;
   @belongsTo('person', { async: true, inverse: 'workLogs' }) person;
   @belongsTo('timesheet', { async: true, inverse: 'workLogs' }) timesheet;
+
+  async clone() {
+    const [task, person, timesheet] = await Promise.all([
+      this.task,
+      this.person,
+      this.timesheet,
+    ]);
+
+    return {
+      uri: this.uri,
+      date: this.date,
+      duration: this.duration,
+      note: this.note,
+      task,
+      person,
+      timesheet,
+    };
+  }
 }
