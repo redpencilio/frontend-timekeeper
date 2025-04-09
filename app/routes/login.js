0670daa6b7f55f292221f192009ba475af476b54
Michaël Dierick <michael.dierick@redpencil.io>
2025-04-09 12:34:03 +0200

    redirect from login when already active session

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class LoginRoute extends Route {}
+export default class LoginRoute extends Route {
+  @service session;
+  @service router;
+
+  async beforeModel() {
+    if (this.session.isAuthenticated) {
+      this.router.transitionTo('index');
+    }
+  }
+}
