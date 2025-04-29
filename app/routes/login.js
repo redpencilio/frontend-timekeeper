81e75ab385f1d419012707125acdf9f38d7529c1
Erika Pauwels <erika.pauwels@gmail.com>
2025-04-29 09:37:54 +0200

    Fix missing newline

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,7 @@ import { service } from '@ember/service';
 
 export default class LoginRoute extends Route {
   @service session;
+
   beforeModel() {
     this.session.prohibitAuthentication('index');
   }
