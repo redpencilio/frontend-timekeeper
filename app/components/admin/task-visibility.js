ab92ad0f9b7a40b114fbe0c9cbbf85c42792880d
Erika Pauwels <erika.pauwels@gmail.com>
2025-04-29 15:52:06 +0200

    Simplify task visibility route

--- app/components/admin/task-visibility.js
+++ app/components/admin/task-visibility.js
@@ -1,104 +1,58 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
-import { trackedFunction } from 'reactiveweb/function';
-import { use } from 'ember-resources';
-import { keepLatest } from 'reactiveweb/keep-latest';
+import { task } from 'ember-concurrency';
 
 export default class AdminTaskVisibilityComponent extends Component {
   @service store;
   @service('tasks') tasksService;
 
-  @use tasksForUserLatest = keepLatest({
-    value: () => this.tasksForUser.value,
-    when: () => this.tasksForUser.isLoading,
-  });
+  get visibilityHierarchy() {
+    return this.tasksService.taskHierarchy.map(({ customer, tasks }) => {
+      const tasksWithVisibility = tasks.map(({ task, subTasks }) => {
+        const subTasksWithVisibilty = subTasks.map((subTask) => {
+          const isVisible = this.args.visibleTasks.includes(subTask);
+          return { task: subTask, isVisible };
+        });
 
-  tasksForUser = trackedFunction(this, async () => {
-    return await this.store.queryAll('task', {
-      'filter[visible-to][:id:]': this.args.person.id,
+        const isVisible = subTasksWithVisibilty.some((task) => task.isVisible);
+        const showSubTasks =
+          subTasksWithVisibilty.length > 1 ||
+          subTasksWithVisibilty[0].task.name !== 'General';
+
+        return {
+          task,
+          isVisible,
+          showSubTasks,
+          subTasks: subTasksWithVisibilty,
+        };
+      });
+
+      const isVisible = tasksWithVisibility.some((task) => task.isVisible);
+      return { customer, isVisible, tasks: tasksWithVisibility };
     });
+  }
+
+  toggleVisibility = task(async (hierarchyItem) => {
+    const targetValue = !hierarchyItem.isVisible;
+    let leafEntries;
+
+    if (hierarchyItem.tasks) {
+      leafEntries = hierarchyItem.tasks.map((task) => task.subTasks).flat();
+    } else if (hierarchyItem.subTasks) {
+      leafEntries = hierarchyItem.subTasks;
+    } else {
+      leafEntries = [hierarchyItem];
+    }
+
+    const leafTasks = leafEntries.map((entry) => entry.task);
+    this.args.onChange(leafTasks, targetValue);
   });
-
-  customerIsVisible = (customerWithTasks) => {
-    return customerWithTasks.tasks.some(this.taskIsVisible);
-  };
-
-  taskIsVisible = (taskWithSubTasks) => {
-    return taskWithSubTasks.subTasks.some(this.subTaskIsVisible);
-  };
-
-  subTaskIsVisible = (subTask) => {
-    return !!this.tasksForUserLatest.find(
-      (userVisibleSubTask) => userVisibleSubTask.id === subTask.id,
-    );
-  };
-
-  doAndRefresh = async (cb) => {
-    await cb();
-    await this.tasksForUser.retry();
-  };
-
-  hideCustomer = async (customerWithTasks) => {
-    const subTasks = customerWithTasks.tasks
-      .map(({ subTasks }) => subTasks)
-      .flat();
-    await this.submitEditVisibleSubTasks(subTasks);
-  };
-
-  hideTask = async (taskWithSubTasks) => {
-    await this.submitEditVisibleSubTasks(taskWithSubTasks.subTasks);
-  };
-
-  hideSubTask = async (subTask) => {
-    await this.submitEditVisibleSubTasks([subTask]);
-  };
-
-  submitEditVisibleSubTasks = async (subTasks, operation = 'hide') => {
-    const visibleTasks = await this.store.queryAll('task', {
-      'filter[visible-to][:id:]': this.args.person.id,
-    });
-    this.args.person.tasks =
-      operation === 'hide'
-        ? visibleTasks
-            .slice()
-            .filter(
-              (visibleTask) =>
-                !subTasks.find((task) => task.id === visibleTask.id),
-            )
-        : visibleTasks.slice().concat(subTasks);
-    await this.args.person.save();
-  };
-
-  showCustomer = async (customerWithTasks) => {
-    const subTasks = customerWithTasks.tasks
-      .map(({ subTasks }) => subTasks)
-      .flat();
-    await this.submitEditVisibleSubTasks(subTasks, 'show');
-  };
-
-  showTask = async (taskWithSubTasks) => {
-    await this.submitEditVisibleSubTasks(taskWithSubTasks.subTasks, 'show');
-  };
-
-  showSubTask = async (subTask) => {
-    await this.submitEditVisibleSubTasks([subTask], 'show');
-  };
 
   showAllTasks = async () => {
-    await this.submitEditVisibleSubTasks(
-      this.tasksService.tasks.slice(),
-      'show',
-    );
+    await this.args.onChange(this.tasksService.leafTasks.slice(0), true);
   };
 
   hideAllTasks = async () => {
-    await this.submitEditVisibleSubTasks(
-      this.tasksService.tasks.slice(),
-      'hide',
-    );
+    await this.args.onChange(this.tasksService.leafTasks.slice(0), false);
   };
-
-  // TODO: Maybe we need to put this in the model file?
-  isSingleGeneral = ({ subTasks }) =>
-    subTasks.length === 1 && subTasks[0].name === 'General';
 }
--- app/routes/admin/users/user/visible-tasks.js
+++ app/routes/admin/users/user/visible-tasks.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class AdminUsersUserVisibleTasksRoute extends Route {
+  @service store;
+
   async model() {
-    const { user } = this.modelFor('admin.users.user');
-    return { user };
+    const user = this.modelFor('admin.users.user');
+
+    const visibleTasks = await this.store.queryAll('task', {
+      'filter[visible-to][:id:]': user.id,
+    });
+
+    return { user, visibleTasks };
   }
 }
